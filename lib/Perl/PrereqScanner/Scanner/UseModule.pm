0adac21214f732112358bc2a68f9248e62db7e91
bowtie <bowtie@cpan.org>
2014-01-11 13:19:45 +0000

    more ttd for ror

--- lib/Perl/PrereqScanner/Scanner/UseModule.pm
+++ lib/Perl/PrereqScanner/Scanner/UseModule.pm
@@ -1,3 +1,4 @@
+use v5.10;
 use strict;
 use warnings;
 
@@ -9,7 +10,7 @@ package Perl::PrereqScanner::Scanner::UseModule;
 use Moo;
 with 'Perl::PrereqScanner::Scanner';
 
-#use Data::Printer; # caller_info => 1;
+use Data::Printer caller_info => 1;
 use Try::Tiny;
 
 =head1 DESCRIPTION
@@ -38,31 +39,107 @@ sub scan_for_prereqs {
 	my @modules;
 	my @version_strings;
 
-#	p $ppi_doc;
-	my $module_runtime_include_found = FALSE;
+# bug out if there is no Include for Module::Runtime found
+	return if $self->_is_module_runtime($ppi_doc) eq FALSE;
 
-	#test for module_ryntime
+	say 'Option 1: use_module( M::N )...';
 
-	my $includes = $ppi_doc->find('PPI::Statement::Include');
-	if ($includes) {
-		foreach my $include (@{$includes}) {
-			next if $include->type eq 'no';
-			if (not $include->pragma) {
-				my $module = $include->module;
-#				p $module;
-				if ($module eq 'Module::Runtime') {
-					$module_runtime_include_found = TRUE;
-				}
-			}
-		}
-	}
+#
+# use_module("Math::BigInt", 1.31)->new("1_234");
+#
+#PPI::Document
+#  PPI::Statement
+#	 PPI::Token::Whitespace  	' '
+#    PPI::Token::Word  	'use_module'
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"Math::BigInt"'
+#        PPI::Token::Operator  	','
+#        PPI::Token::Whitespace  	' '
+#        PPI::Token::Number::Float  	'1.31'
+#    PPI::Token::Operator  	'->'
+#    PPI::Token::Word  	'new'
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"1_234"'
+#    PPI::Token::Structure  	';'
+#  PPI::Token::Whitespace  	'\n'
 
-#	p $module_runtime_include_found;
-	return if $module_runtime_include_found eq FALSE;
-#	say 'checking for use_module';
+	try {
+		my @chunks1 =
 
-#say 'Option 1';
-try {
+			map { [$_->schildren] } grep {
+			$_->{children}[0]->content
+				=~ m{\A(?:use_module|use_package_optimistically|require_module)\z}
+			} grep { $_->child(0)->isa('PPI::Token::Word') }
+
+			@{$ppi_doc->find('PPI::Statement') || []};
+
+#	p @chunks1;
+		push @modules, $self->_module_names_psi(@chunks1);
+
+
+	};
+
+
+	say 'Option 2: my $q = use_module( M::N )...';
+
+
+#
+# my $bi = use_module("Math::BigInt", 1.31)->new("1_234");
+#
+#PPI::Document
+#  PPI::Statement::Variable
+#    PPI::Token::Word  	'my'
+#    PPI::Token::Whitespace  	' '
+#    PPI::Token::Symbol  	'$bi'
+#    PPI::Token::Whitespace  	' '
+#    PPI::Token::Operator  	'='
+#    PPI::Token::Whitespace  	' '
+#    PPI::Token::Word  	'use_module'
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"Math::BigInt"'
+#        PPI::Token::Operator  	','
+#        PPI::Token::Whitespace  	' '
+#        PPI::Token::Number::Float  	'1.31'
+#    PPI::Token::Operator  	'->'
+#    PPI::Token::Word  	'new'
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"1_234"'
+#    PPI::Token::Structure  	';'
+#  PPI::Token::Whitespace  	'\n'
+
+	try {
+		my @chunks2 =
+
+			map { [$_->schildren] }
+
+			grep {
+			$_->{children}[6]->content
+				=~ m{\A(?:use_module)\z}
+			} grep { $_->child(6)->isa('PPI::Token::Word') }
+
+			grep { $_->child(4)->content eq '=' }
+			grep { $_->child(4)->isa('PPI::Token::Operator') }
+
+			grep { $_->child(2)->isa('PPI::Token::Symbol') }
+
+			grep { $_->{children}[0]->content eq 'my' }
+			grep { $_->child(0)->isa('PPI::Token::Word') }
+
+			@{$ppi_doc->find('PPI::Statement::Variable') || []}
+			;    # need for pps remove in midgen -> || {}
+
+#	p @chunks1;
+		push @modules, $self->_module_names_psi(@chunks2);
+
+
+	};
+
+	say 'Option 3: $q = use_module( M::N )...';
+
 #
 # $bi = use_module("Math::BigInt", 1.31)->new("1_234");
 #
@@ -87,69 +164,33 @@ try {
 #    PPI::Token::Structure  	';'
 #  PPI::Token::Whitespace  	'\n'
 
+	try {
+		my @chunks3 =
 
-	my @chunks =
+			map { [$_->schildren] }
 
-		map  { [$_->schildren] }
-#		grep { $_->{children}[4]->content eq 'use_module' }
-#		grep { $_->child(4)->literal =~ m{\A(?:use_module|use_package_optimistically)\z} }
-#		grep { $_->{children}[4]->content eq 'use_module' || 'use_package_optimistically' }
-		grep { $_->{children}[4]->content =~ m{\A(?:use_module|use_package_optimistically)\z} }
+			grep {
+			$_->{children}[4]->content
+				=~ m{\A(?:use_module)\z}
+			} grep { $_->child(4)->isa('PPI::Token::Word') }
+
+			grep { $_->child(2)->content eq '=' }
+			grep { $_->child(2)->isa('PPI::Token::Operator') }
+
+			grep { $_->child(0)->isa('PPI::Token::Symbol') }
+
+			@{$ppi_doc->find('PPI::Statement') || []}
+			;    # need for pps remove in midgen -> || {}
+
+#	p @chunks3;
+		push @modules, $self->_module_names_psi(@chunks3);
 
 
+	};
 
-		grep { $_->child(4)->isa('PPI::Token::Word') }
 
-	    grep { $_->child(2)->content eq '=' }
-	    grep { $_->child(2)->isa('PPI::Token::Operator') }
+	say 'Option 4: return use_module( M::N )...';
 
-		grep { $_->child(0)->isa('PPI::Token::Symbol') }
-
-		@{$ppi_doc->find('PPI::Statement') || []}; # need for pps remove in midgen -> || {}
-
-#	p @chunks;
-
-	foreach my $hunk (@chunks) {
-
-#		p $hunk;
-
-		# looking for use Module::Runtime ...;
-		if (grep { $_->isa('PPI::Structure::List') } @$hunk) {
-
-#			say 'found Module::Runtime';
-
-			# hack for List
-			my @hunkdata = @$hunk;
-
-			foreach my $ppi_sl (@hunkdata) {
-				if ($ppi_sl->isa('PPI::Structure::List')) {
-#					p $ppi_sl;
-					foreach my $ppi_se (@{$ppi_sl->{children}}) {
-						if ($ppi_se->isa('PPI::Statement::Expression')) {
-							foreach my $element (@{$ppi_se->{children}}) {
-								if ( $element->isa('PPI::Token::Quote::Single')
-									|| $element->isa('PPI::Token::Quote::Double'))
-								{
-									my $module = $element;
-									$module =~ s/^['|"]//;
-									$module =~ s/['|"]$//;
-									if ($module =~ m/\A[A-Z]/) {
-										push @modules, $module;
-									}
-								}
-
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-};
-
-#say 'Option 2';
-try{
-#	say 'lets check for require use_module';
 #
 # return use_module(\'App::SCS::PageSet\')->new(
 # base_dir => $self->share_dir->catdir(\'pages\'),
@@ -197,62 +238,51 @@ try{
 #      PPI::Token::Whitespace  	'  '
 #    PPI::Token::Structure  	';'
 
+	try {
+		my @chunks4 =
 
+			map { [$_->schildren] }
 
-	my @chunks =
-
-		map  { [$_->schildren] }
-		grep { $_->{children}[2]->content eq 'use_module' || 'use_package_optimistically' }
+#		grep { $_->{children}[2]->content eq 'use_module' || 'use_package_optimistically' }
 #		grep { $_->child(2)->literal =~ m{\A(?:use_module|use_package_optimistically)\z} }
-#		grep { $_->{children}[2]->content =~ m{\A(?:use_module|use_package_optimistically)\z} }
-
-		grep { $_->child(2)->isa('PPI::Token::Word') }
-
-	    grep { $_->child(0)->content =~ m{\A(?:return)\z} }
-		grep { $_->child(0)->isa('PPI::Token::Word') }
-
-		@{$ppi_doc->find('PPI::Statement::Break') || []};
-
-#	p @chunks;
-
-	foreach my $hunk (@chunks) {
-
-#		p $hunk;
-
-		# looking for use Module::Runtime ...;
-		if (grep { $_->isa('PPI::Structure::List') } @$hunk) {
-
-#			say 'found Module::Runtime';
-
-			# hack for List
-			my @hunkdata = @$hunk;
-
-			foreach my $ppi_sl (@hunkdata) {
-				if ($ppi_sl->isa('PPI::Structure::List')) {
-#					p $ppi_sl;
-					foreach my $ppi_se (@{$ppi_sl->{children}}) {
-						if ($ppi_se->isa('PPI::Statement::Expression')) {
-							foreach my $element (@{$ppi_se->{children}}) {
-								if ( $element->isa('PPI::Token::Quote::Single')
-									|| $element->isa('PPI::Token::Quote::Double'))
-								{
-									my $module = $element;
-									$module =~ s/^['|"]//;
-									$module =~ s/['|"]$//;
-									if ($module =~ m/\A[A-Z]/) {
-										push @modules, $module;
-									}
-								}
-
-							}
-						}
-					}
-				}
+			grep {
+			$_->{children}[2]->content
+				=~ m{\A(?:use_module|use_package_optimistically)\z}
 			}
-		}
-	}
-};
 
+			grep { $_->child(2)->isa('PPI::Token::Word') }
+
+#	    grep { $_->child(0)->content =~ m{\A(?:return)\z} }
+			grep { $_->child(0)->content =~ m{(?:return)} }
+			grep { $_->child(0)->isa('PPI::Token::Word') }
+
+			@{$ppi_doc->find('PPI::Statement::Break') || []};
+
+#	p @chunks3;
+		push @modules, $self->_module_names_psi(@chunks4);
+
+
+	};
+
+
+
+#	p @modules         if $self->debug;
+#	p @version_strings if $self->debug;
+
+	# if we found a module, process it with the correct catogery
+#	if (scalar @modules > 0) {
+
+#		if ( $self->format =~ /cpanfile|metajson/ ) {
+#			if ( $self->xtest eq 'test_requires' ) {
+#				$self->_process_found_modules( 'test_requires', \@modules );
+#			} elsif ( $self->develop && $self->xtest eq 'test_develop' ) {
+#				$self->_process_found_modules( 'test_develop', \@modules );
+#			}
+#		} else {
+#		$self->_process_found_modules('requires_suggests', \@modules);
+
+#		}
+#	}
 
 
 	foreach (0 .. $#modules) {
@@ -263,13 +293,14 @@ try{
 }
 
 
+
+
 #######
 # composed method test for include Module::Runtime
 #######
 sub _is_module_runtime {
 	my ($self, $doc) = @_;
 	my $module_runtime_include_found = FALSE;
-	p $doc;
 
 #PPI::Document
 #  PPI::Statement::Include
@@ -284,7 +315,6 @@ sub _is_module_runtime {
 				next if $include->type eq 'no';
 				if (not $include->pragma) {
 					my $module = $include->module;
-					p $module;
 					if ($module eq 'Module::Runtime') {
 						$module_runtime_include_found = TRUE;
 					}
@@ -292,11 +322,78 @@ sub _is_module_runtime {
 			}
 		}
 	};
-	p $module_runtime_include_found;
 	return $module_runtime_include_found;
 
 }
 
+
+#######
+# composed method extract module name from PPI::Structure::List
+#######
+sub _module_names_psi {
+	my $self   = shift;
+	my @chunks = @_;
+	my @modules_psl;
+
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"Math::BigInt"'
+#        PPI::Token::Operator  	','
+#        PPI::Token::Whitespace  	' '
+#        PPI::Token::Number::Float  	'1.31'
+#    PPI::Token::Operator  	'->'
+#    PPI::Token::Word  	'new'
+#    PPI::Structure::List  	( ... )
+#      PPI::Statement::Expression
+#        PPI::Token::Quote::Double  	'"1_234"'
+#    PPI::Token::Structure  	';'
+#  PPI::Token::Whitespace  	'\n'
+
+
+	try {
+		foreach my $hunk (@chunks) {
+
+#		p $hunk;
+
+			# looking for use Module::Runtime ...;
+			if (grep { $_->isa('PPI::Structure::List') } @$hunk) {
+
+#			say 'found Module::Runtime';
+
+				# hack for List
+				my @hunkdata = @$hunk;
+
+				foreach my $ppi_sl (@hunkdata) {
+					if ($ppi_sl->isa('PPI::Structure::List')) {
+
+#					p $ppi_sl;
+						foreach my $ppi_se (@{$ppi_sl->{children}}) {
+							if ($ppi_se->isa('PPI::Statement::Expression')) {
+								foreach my $element (@{$ppi_se->{children}}) {
+									if ( $element->isa('PPI::Token::Quote::Single')
+										|| $element->isa('PPI::Token::Quote::Double'))
+									{
+										my $module = $element;
+										$module =~ s/^['|"]//;
+										$module =~ s/['|"]$//;
+										if ($module =~ m/\A[A-Z]/) {
+											push @modules_psl, $module;
+											p @modules_psl;    #         if $self->debug;
+										}
+									}
+
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	};
+
+	return @modules_psl;
+
+}
 
 1;
 
--- t/scanner-use_module.t
+++ t/scanner-use_module.t
@@ -10,85 +10,163 @@ use Try::Tiny;
 use Test::More;
 
 sub prereq_is {
-  my ($str, $want, $comment) = @_;
-  $comment ||= $str;
+	my ($str, $want, $comment) = @_;
+	$comment ||= $str;
 
-  my $scanner = Perl::PrereqScanner->new({scanners => [qw( UseModule )]});
+	my $scanner = Perl::PrereqScanner->new({scanners => [qw( UseModule )]});
 
-  # scan_ppi_document
-  try {
-    my $result = $scanner->scan_ppi_document(PPI::Document->new(\$str));
-    is_deeply($result->as_string_hash, $want, $comment);
-  }
-  catch {
-    fail("scanner died on: $comment");
-    diag($_);
-  };
+	# scan_ppi_document
+	try {
+		my $result = $scanner->scan_ppi_document(PPI::Document->new(\$str));
+		is_deeply($result->as_string_hash, $want, $comment);
+	}
+	catch {
+		fail("scanner died on: $comment");
+		diag($_);
+	};
 
-  # scan_string
-  try {
-    my $result = $scanner->scan_string($str);
-    is_deeply($result->as_string_hash, $want, $comment);
-  }
-  catch {
-    fail("scanner died on: $comment");
-    diag($_);
-  };
+	# scan_string
+	try {
+		my $result = $scanner->scan_string($str);
+		is_deeply($result->as_string_hash, $want, $comment);
+	}
+	catch {
+		fail("scanner died on: $comment");
+		diag($_);
+	};
 
-  # scan_file
-  try {
-    my ($fh, $filename) = tempfile(UNLINK => 1);
-    print $fh $str;
-    close $fh;
-    my $result = $scanner->scan_file($filename);
-    is_deeply($result->as_string_hash, $want, $comment);
-  }
-  catch {
-    fail("scanner died on: $comment");
-    diag($_);
-  };
+	# scan_file
+	try {
+		my ($fh, $filename) = tempfile(UNLINK => 1);
+		print $fh $str;
+		close $fh;
+		my $result = $scanner->scan_file($filename);
+		is_deeply($result->as_string_hash, $want, $comment);
+	}
+	catch {
+		fail("scanner died on: $comment");
+		diag($_);
+	};
 }
 
 prereq_is('', {}, '(empty string)');
 
-prereq_is('use Data::Printer;
+prereq_is(
+	'use Data::Printer;
 use Foo::Bar;
-', {}, '(not Module::Runtime)');
+', {}, '(no Module::Runtime)'
+);
 
-prereq_is('use Data::Printer;
+prereq_is(
+	'use Data::Printer;
 use Module::Runtime;
 use Foo::Bar;
-', {}, '(Module::Runtime)');
+', {}, '(use Module::Runtime)'
+);
 
-prereq_is('use Module::Runtime;
+prereq_is(
+	'use Module::Runtime;
+use_module("Math::BigInt", 1.31)->new("1_234");
+', {'Math::BigInt' => 0}, '("use_module( M::N )...")'
+);
+
+prereq_is(
+	'use Module::Runtime;
+require_module("Math::BigInt");
+', {'Math::BigInt' => 0}, '("require_module( R::M )")'
+);
+
+prereq_is(
+	'use Module::Runtime;
+use_package_optimistically("Math::BigInto", 1.234)->new("1_234");
+', {'Math::BigInto' => 0}, '("use_package_optimistically( P::M )...")'
+);
+
+prereq_is(
+	'use Module::Runtime;
+    require_module(\'B::Hooks::EndOfScope::PP::HintHash\');
+', {'B::Hooks::EndOfScope::PP::HintHash' => 0}, '("require_module( B::Hooks::EndOfScope::PP::HintHash )"'
+);
+
+prereq_is(
+	'use Module::Runtime;
+BEGIN {
+  if (_PERL_VERSION =~ /^5\.009/) {
+    # CBA to figure out where %^H got broken and which H::U::HH is sane enough
+    die "By design B::Hooks::EndOfScope does not operate in pure-perl mode on perl 5.9.X\n"
+  }
+  elsif (_PERL_VERSION < \'5.010\') {
+    require_module(\'B::Hooks::EndOfScope::PP::HintHash\');
+    *on_scope_end = \&B::Hooks::EndOfScope::PP::HintHash::on_scope_end;
+  }
+  else {
+    require_module(\'B::Hooks::EndOfScope::PP::FieldHash\');
+    *on_scope_end = \&B::Hooks::EndOfScope::PP::FieldHash::on_scope_end;
+  }
+}
+',
+{'B::Hooks::EndOfScope::PP::HintHash' => 0, 'B::Hooks::EndOfScope::PP::FieldHash' => 0,}, '("require_module( B::Hooks )"'
+);
+
+#prereq_is(
+#	'use Module::Runtime;
+#$bi = use_package_optimistically("Math::BigInto", 1.234)->new("1_234");
+#', {'Math::BigInto' => 0}, '("Math::BigInto", 1.234)'
+#);
+
+
+prereq_is(
+	'use Module::Runtime;
+my $bi = use_module("Math::BigInt", 1.31)->new("1_234");
+', {'Math::BigInt' => 0}, '("my $bi = use_module( M::N )...")'
+);
+
+prereq_is(
+	'use Module::Runtime;
 $bi = use_module("Math::BigInt", 1.31)->new("1_234");
-', {'Math::BigInt' => 0}, '("Math::BigInt", 1.31)');
+', {'Math::BigInt' => 0}, '("$bi = use_module( M::N )...")'
+);
 
-prereq_is('use Module::Runtime;
-$bi = use_package_optimistically("Math::BigInto", 1.234)->new("1_234");
-', {'Math::BigInto' => 0}, '("Math::BigInto", 1.234)');
 
-prereq_is('use Module::Runtime;
+
+prereq_is(
+	'use Module::Runtime;
 return use_module(\'App::SCS::PageSet\')->new(
 base_dir => $self->share_dir->catdir(\'pages\'),
 plugin_config => $self->page_plugin_config,
 );
-', {'App::SCS::PageSet' => 0}, '("App::SCS::PageSet", 0)');
-
-prereq_is('use Module::Runtime;
-return use_package_optimistically(\'App::SCS::PageSeto\')->new(
-base_dir => $self->share_dir->catdir(\'pages\'),
-plugin_config => $self->page_plugin_config,
+', {'App::SCS::PageSet' => 0}, '("return use_module( M::N )...")'
 );
-', {'App::SCS::PageSeto' => 0}, '("App::SCS::PageSeto", 0)');
 
-prereq_is('use Module::Runtime;
-return use_module("App::SCS::Web")->new(app => $self);
-', {'App::SCS::Web' => 0}, '("App::SCS::Web", 0)');
-
-prereq_is('use Module::Runtime;
-return use_package_optimisticall("App::SCS::Webo")->new(app => $self);
-', {'App::SCS::Webo' => 0}, '("App::SCS::Webo", 0)');
+#prereq_is(
+#	'use Module::Runtime;
+#return use_package_optimistically(\'App::SCS::PageSeto\')->new(
+#base_dir => $self->share_dir->catdir(\'pages\'),
+#plugin_config => $self->page_plugin_config,
+#);
+#', {'App::SCS::PageSeto' => 0}, '("App::SCS::PageSeto", 0)'
+#);
+#
+#prereq_is(
+#	'use Module::Runtime;
+#return use_module("App::SCS::Web")->new(app => $self);
+#', {'App::SCS::Web' => 0}, '("App::SCS::Web", 0)'
+#);
+#
+#prereq_is(
+#	'use Module::Runtime;
+#return use_package_optimisticall("App::SCS::Webo")->new(app => $self);
+#', {'App::SCS::Webo' => 0}, '("App::SCS::Webo", 0)'
+#);
+#
+#prereq_is(
+#	'use Module::Runtime;
+#  return use_module(\'App::SCS::PageSetxx\')->new(
+#    base_dir => $self->share_dir->catdir(\' pages \'),
+#    plugin_config => $self->page_plugin_config,
+#  );
+#', {'App::SCS::PageSetxx' => 0}, '("App::SCS::Pagesetxx", 0)'
+#);
 
 
 #prereq_is('use Module::Runtime;
